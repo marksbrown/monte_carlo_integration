505f658507f7dc74330539fbc125a998c764ac34
Mark S. Brown <sandcalc@gmail.com>
2022-03-14 13:53:42 +0000

    updated and  finished (to my mind)

--- main.c
+++ main.c
@@ -1,64 +1,68 @@
 #include "montecarlo.h"
-
-int partial_torus_condition(point *apoint){
-
-    double x = apoint->x;
-    double y = apoint->y;
-    double z = apoint->z;
-
-    double R = pow(x, 2) + pow(y, 2);
-    double r = pow(z - 1, 2) + pow(pow(R, 0.5) - 3, 2);
-    if (x >= 1 && y >= -3 && r <= 1){
-        return 1;
-    }else {
-        return 0;
-    }
-}
-
-result partial_torus(int N){
-    mc torus;
-    torus.condition = partial_torus_condition;
-    torus.next_point = randomise_point;
-    
-    torus.limits.x.l = -4;
-    torus.limits.x.h = 4;
-    torus.limits.y.l = -4;
-    torus.limits.y.h = 4;
-    torus.limits.z.l = 0;
-    torus.limits.z.h = 2;
-    
-    return com_monte_carlo(N, &torus);
-}
+#include "cases.h"
 
 double absolute_deviation(double predicted, double expected){
     return fabs(predicted - expected);
 }
 
-void partial_torus_deviation(int N){
-    /* print the deviations in all four values from expected!*/
-    double expected_volume;
-    expected_volume = 22.194176;
 
-    point expected_COM;
-    expected_COM.x = 2.4076; 
-    expected_COM.y = 0.16210;
-    expected_COM.z = 1.0;
+result monte_carlo_deviation(int N, result *expected, result (*monte_carlo)(int)){
+    /* Determine Deviation from Expected */
     
-    result r = partial_torus(N);
-    //printf("Volume : %f\n COM : (%f, %f, %f)\n", r.volume, r.COM.x, r.COM.y, r.COM.z);
-    double dV = absolute_deviation(expected_volume, r.volume);   
-    double dx = absolute_deviation(expected_COM.x, r.COM.x);
-    double dy = absolute_deviation(expected_COM.y, r.COM.y);
-    double dz = absolute_deviation(expected_COM.y, r.COM.z);
-    printf("%d,%f,%f,%f,%f\n", N, dV, dx, dy, dz);
+    result r = (monte_carlo)(N);
+    r.volume = absolute_deviation(expected->volume, r.volume);   
+    r.COM.x = absolute_deviation(expected->COM.x, r.COM.x);
+    r.COM.y = absolute_deviation(expected->COM.y, r.COM.y);
+    r.COM.z = absolute_deviation(expected->COM.y, r.COM.z);
+    return r;
+}
+
+result expected_partial_torus(){
+    result r;
+    r.volume = 22.194176;
+    r.COM.x = 2.4076; 
+    r.COM.y = 0.16210;
+    r.COM.z = 0.0;
+    return r;
+}
+
+result expected_sphere(){
+    result r;
+    r.volume = 4.1887902047863905;
+    r.COM.x = 1.0;
+    r.COM.y = 1.0;
+    r.COM.z = 1.0;
+    return r;
+}
+
+void add_results(result *f, result *s){
+    f->volume += s->volume;
+    f->COM.x += s->COM.x;
+    f->COM.y += s->COM.y;
+    f->COM.z += s->COM.z;
 }
 
 int main(){
+    //result e = expected_partial_torus();
+    result e = expected_sphere();
     int j;
     int N;
-    for (j = 3; j < 40; j++){
+    int repeats;
+    int M;
+    M = 10;
+    result r;
+    for (j = 5; j < 30; j++){
         N = (int) pow(1.5, j);
-        partial_torus_deviation(N);
+        for (repeats = 0; repeats < M; repeats ++){
+            
+            result new_r = monte_carlo_deviation(N, &e, &sphere);
+            if (repeats == 0){
+                r = new_r;
+            } else {
+                add_results(&r, &new_r);
+            }
+        }
+        printf("%d,%f,%f,%f,%f\n", N, r.volume / M, r.COM.x / M, r.COM.y / M, r.COM.z / M);
     }
     
 }
